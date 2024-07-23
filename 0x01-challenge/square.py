449bc2bdd1f3b9a01aab4b56d54cbd14711ed0d8
[esekyi] <[sskert10@gmail.com]>
2024-07-23 03:01:12 +0000

    fixes - square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -11,7 +11,7 @@ class square():
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
         """ Permiter of the square """
